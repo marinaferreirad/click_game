f65e28794bcd0d618e63e43b2bf0fad7c20fc052
Marina Ferreira Duarte <marina.ferreira.d@hotmail.fr>
2016-12-13 13:33:44 +0100

    tabs_wip

--- src/js/script.js
+++ src/js/script.js
@@ -14,6 +14,27 @@ button.onclick = function() {
 };
 
 
+var tabs = document.querySelectorAll('.tabs a')
+
+for (var i = 0; i < tabs.length; i++){
+  tabs[i].addEventListener('click', function(e){
+    
+    var li = this.parentNode
+    var div = this.parentNode.parentNode.parentNode
+    
+    if (li.classList.contains('active')){
+      return false
+    }
+    
+    div.querySelector('.tabs .active').classList.remove('active')
+    li.classList.add('active')
+    
+    div.querySelector('.tab-content.active').classList.remove('active')
+    div.querySelector(this.getAttribute('href')).classList.add('active')
+  })
+}
+
+
 // Variables objets :
 
 var van = new Object();
